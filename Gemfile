8a9b3114317159e1af6d24b03e6b7f3e7342062c
Anvi Alex <epononanvialex@gmail.com>
2021-03-17 00:59:54 +0200

    Fix linter errors

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+gem 'nokogiri'
+gem 'rack', '~> 2.0.1'
+gem 'rspec'
+gem 'rubocop', '~>0.81.0'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,12 +1,12 @@
 # This is my first approach of bubble sort in Ruby
 # I have created a boolean variable to check after each iteration the array was modified or not.
 
-def bubble_sort(array)
+def bubble_sort_by(array)
   is_sorted = false
   until is_sorted
     is_sorted = true
     for i in 1...array.length - 1 ## I used three dots for the range to not count the last element of the array
-      if array[i] > array[i + 1]
+      if yield(array[i], array[i + 1]).positive?
         array[i], array[i + 1] = array[i + 1], array[i] # This is my implementation to swap elements
         is_sorted = false
       end
@@ -15,7 +15,6 @@ def bubble_sort(array)
   array
 end
 
-numbers = [5, 1, 67, 35, 10, 2, 100, 20, 45]
-
-list = bubble_sort(numbers)
-puts list
+bubble_sort_by([5, 1, 67, 35, 10, 2, 100, 20, 45]) do |left, right|
+  left - right
+end
