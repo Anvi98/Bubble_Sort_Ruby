b3826957d10a761233a73a5600136f26891e07a1
Anvi Alex <epononanvialex@gmail.com>
2021-03-17 02:10:35 +0200

    Fix while loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,19 +5,24 @@ def bubble_sort(array)
   is_sorted = false
   until is_sorted
     is_sorted = true
-    i = 1
+    i = 0
     while i <= array.length - 2 ## I used three dots for the range to not count the last element of the array
       if array[i] > array[i + 1]
-        array[i], array[i + 1] = array[i + 1], array[i] # This is my implementation to swap elements
+        swap(i, i + 1, array)
         is_sorted = false
       end
       i += 1
     end
+
   end
   array
 end
 
-numbers = [5, 1, 67, 35, 10, 2, 100, 20, 45]
+def swap(i, j, array)
+  array[i], array[j] = array[j], array[i] # This is my implementation to swap elements
+end
+
+numbers = [1, 4, 0, -3, 40]
 
 list = bubble_sort(numbers)
-puts list
+p list
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -5,12 +5,11 @@ def bubble_sort_by(array)
   is_sorted = false
   until is_sorted
     is_sorted = true
-    i = 1
+    i = 0
     while i <= array.length - 2 ## I used three dots for the range to not count the last element of the array
-      if yield(array[i], array[i + 1]).positive?
-        array[i], array[i + 1] = array[i + 1], array[i] # This is my implementation to swap elements
-        is_sorted = false
-      end
+      yield(array[i], array[i + 1]).positive?
+      array[i], array[i + 1] = array[i + 1], array[i] # This is my implementation to swap elements
+      is_sorted = false
       i += 1
     end
   end
@@ -20,3 +19,9 @@ end
 bubble_sort_by(%w([hi hello hey])) do |left, right|
   left.length - right.length
 end
+
+
+list = ["Hi", "Hello", "hey"]
+
+sort = bubble_sort_by(list)
+puts sort
